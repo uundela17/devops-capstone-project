5124470e7bd72bdd6694d7bfa2759355ebcec503
uundela17 <167723440+uundela17@users.noreply.github.com>
2024-04-26 09:09:54 +0530

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -123,4 +123,15 @@ class TestAccountService(TestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
-    # ADD YOUR TEST CASES HERE ...
+     def test_security_headers(self):
+      """It should return security headers"""
+      response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+      self.assertEqual(response.status_code, status.HTTP_200_OK)
+      headers = {
+          'X-Frame-Options': 'SAMEORIGIN',
+          'X-Content-Type-Options': 'nosniff',
+          'Content-Security-Policy': 'default-src \'self\'; object-src \'none\'',
+          'Referrer-Policy': 'strict-origin-when-cross-origin'
+      }
+      for key, value in headers.items():
+          self.assertEqual(response.headers.get(key), value)
