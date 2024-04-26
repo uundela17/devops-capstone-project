bd52aaac1e183da1d45163b2c206be3a48416386
uundela17 <167723440+uundela17@users.noreply.github.com>
2024-04-26 09:12:08 +0530

    Update __init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,12 +1,6 @@
-HTTP/1.1 302 FOUND
-Server: gunicorn
-Date: Thu, 13 Oct 2022 20:00:01 GMT
-Connection: close
-Content-Type: text/html; charset=utf-8
-Content-Length: 233
-Location: https://localhost:5000
-Permissions-Policy: browsing-topics=()
-X-Frame-Options: SAMEORIGIN
-X-Content-Type-Options: nosniff
-Content-Security-Policy: default-src 'self'; object-src 'none'
-Referrer-Policy: strict-origin-when-cross-origin
+    def test_cors_security(self):
+        """It should return a CORS header"""
+        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # Check for the CORS header
+        self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
