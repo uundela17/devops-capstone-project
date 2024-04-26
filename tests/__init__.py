9aac7af7ac8732d8dbb1928f8ffb102046f98ff4
uundela17 <167723440+uundela17@users.noreply.github.com>
2024-04-26 09:10:39 +0530

    Update __init__.py

---
+++ tests/__init__.py
@@ -0,0 +1,12 @@
+HTTP/1.1 302 FOUND
+Server: gunicorn
+Date: Thu, 13 Oct 2022 20:00:01 GMT
+Connection: close
+Content-Type: text/html; charset=utf-8
+Content-Length: 233
+Location: https://localhost:5000
+Permissions-Policy: browsing-topics=()
+X-Frame-Options: SAMEORIGIN
+X-Content-Type-Options: nosniff
+Content-Security-Policy: default-src 'self'; object-src 'none'
+Referrer-Policy: strict-origin-when-cross-origin
